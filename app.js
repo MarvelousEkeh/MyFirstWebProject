9c8ac70e08b76c2c1c4543fabd475d606314c985
tl <tl@PC7-156.njitdm.campus.njit.edu>
2017-11-18 16:47:35 -0500

    Made Changes

---
+++ app.js
@@ -0,0 +1,36 @@
+// var name = prompt("What is your name");
+//
+// function checkName(nameCheck){
+//   if(nameCheck == "Jimmy"){
+//      alert("Congratulations");
+//    }
+//    if(nameCheck == "Marvelous"){
+//    }
+//    else{
+//      alert("Get Out");
+//    }
+// }
+//
+//  checkName(name);
+//
+//  var age = prompt("Enter your age:");
+//
+//  function ageCalculator(personAge){
+//
+//    if(personAge >=0 && personAge <=12){
+//      alert("You are a baby");
+//    }
+//    else if(personAge >=13 && personAge < 20 )
+//    {
+//      alert("You are a teen");
+//    }
+//    else if(personAge >=20 && personAge < 30)
+//    {
+//    alert("You are a young dude");
+//    }
+//    else{
+//      alert("You are almost dead");
+//    }
+//  }
+//
+//  ageCalculator(age);
